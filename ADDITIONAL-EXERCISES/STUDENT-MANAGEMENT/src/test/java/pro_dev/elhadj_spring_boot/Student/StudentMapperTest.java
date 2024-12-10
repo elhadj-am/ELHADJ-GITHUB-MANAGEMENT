f03837a5df79ba41d8ec14eb49161033f3073a64
Elhadj <46904847+elhadj-am@users.noreply.github.com>
2024-12-10 01:43:51 +0100

    Testing "shouldMapStudentDtoToStudent"

--- ADDITIONAL-EXERCISES/STUDENT-MANAGEMENT/src/test/java/pro_dev/elhadj_spring_boot/Student/StudentMapperTest.java
+++ ADDITIONAL-EXERCISES/STUDENT-MANAGEMENT/src/test/java/pro_dev/elhadj_spring_boot/Student/StudentMapperTest.java
@@ -1,6 +1,8 @@
 package pro_dev.elhadj_spring_boot.Student;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 
 class StudentMapperTest {
     StudentMapper studentMapper;
@@ -8,6 +10,20 @@ class StudentMapperTest {
     @BeforeEach
     void setUp() {
         studentMapper = new StudentMapper();
-        
+    }
+
+    @Test
+    public void shouldMapStudentDtoToStudent() {
+        StudentDto studentDto = new StudentDto("Travis",
+                "DIALLO",
+                "travis@gmail.com",
+                1);
+        Student student = studentMapper.toStudent(studentDto);
+
+        Assertions.assertEquals(student.getFirstname(), studentDto.firstname());
+        Assertions.assertEquals(student.getLastname(), studentDto.lastname());
+        Assertions.assertEquals(student.getEmail(), studentDto.email());
+        Assertions.assertNotNull(student.getSchool().getId());
+        Assertions.assertEquals(student.getSchool().getId(), studentDto.schoolId());
     }
 }
