8cabbcbe4ca198e3dc83c667df162f230790870e
Elhadj <46904847+elhadj-am@users.noreply.github.com>
2024-12-20 18:26:55 +0100

    Merge of the ongoing development work

--- TO-DO-LIST-APP/src/main.jsx
+++ TO-DO-LIST-APP/src/main.jsx
@@ -1,9 +1,11 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import Todo from "./components/Todo";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>,
-)
+    <Todo />
+  </React.StrictMode>
+);
