68b1bc0da78881fe786331eec3d61728ffc6423e
Elhadj <46904847+elhadj-am@users.noreply.github.com>
2024-12-22 20:32:08 +0100

    Merge of the ongoing development work
    Co-Authored-By: spartans1910 <171082753+spartans1910@users.noreply.github.com>

--- TO-DO-LIST-APP/src/components/Form.jsx
+++ TO-DO-LIST-APP/src/components/Form.jsx
@@ -1,4 +1,5 @@
 import { useState } from "react";
+import styles from "./Form.module.css";
 export default function Form({ todos, setTodos }) {
   const [todo, setTodo] = useState("");
 
@@ -8,13 +9,19 @@ export default function Form({ todos, setTodos }) {
     setTodo("");
   }
   return (
-    <form onSubmit={handleSubmit}>
-      <input
-        type="text"
-        value={todo}
-        onChange={(e) => setTodo(e.target.value)}
-      />
-      <button type="submit">Add</button>
+    <form className={styles.todoform} onSubmit={handleSubmit}>
+      <div className="flex">
+        <input
+          className={styles.modernInput}
+          type="text"
+          value={todo}
+          onChange={(e) => setTodo(e.target.value)}
+          placeholder="Enter a Todo Item "
+        />
+        <button className={styles.modernButton} type="submit">
+          Add
+        </button>
+      </div>
     </form>
   );
 }
