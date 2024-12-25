f2062c4458e49a25d41df1fa38931989740c44a8
Elhadj <46904847+elhadj-am@users.noreply.github.com>
2024-12-25 21:00:38 +0100

    Todo list develop (#19)
    * Cleaning, 1, 2, 3, let's go
    
    * Create Todo.jsx
    
    * Merge of the ongoing development work
    
    * Merge of the ongoing development work
    
    * Merge of the ongoing development work
    
    * Merge of the ongoing development work
    
    Co-Authored-By: spartans1910 <171082753+spartans1910@users.noreply.github.com>
    
    * update Readme
    
    Co-Authored-By: spartans1910 <171082753+spartans1910@users.noreply.github.com>
    
    * pull from main
    
    * Merge of the ongoing development work
    
    ---------
    
    Co-authored-by: spartans1910 <171082753+spartans1910@users.noreply.github.com>

--- TO-DO-LIST-APP/src/components/Header.jsx
+++ TO-DO-LIST-APP/src/components/Header.jsx
@@ -1,5 +1,5 @@
 import styles from "./header.module.css";
 
 export default function Header() {
-  return <div className={styles.header}>My header</div>;
+  return <div className={styles.header}>MY TODO LIST</div>;
 }
--- TO-DO-LIST-APP/src/components/TodoItem.jsx
+++ TO-DO-LIST-APP/src/components/TodoItem.jsx
@@ -1,3 +1,16 @@
+import styles from "./todoitem.module.css";
+
 export default function TodoItem({ x }) {
-  return <h3>{x}</h3>;
+  return (
+    <div className={styles.item}>
+      <div className={styles.itemName}>
+        {x}
+        <span>
+          <button className={styles.deleteButton}>x</button>
+        </span>
+      </div>
+
+      <hr className={styles.line} />
+    </div>
+  );
 }
